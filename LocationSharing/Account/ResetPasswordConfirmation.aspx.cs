ac7e956a626e8889cc8c6d8305a5e8e2ab40bcb5
smfarhan <smfarhan@ISB-AI-FARHAN>
2016-11-20 21:38:56 +0500

    Add project files.

---
+++ LocationSharing/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace LocationSharing.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ LocationSharing/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LocationSharing.Startup))]
+namespace LocationSharing
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
